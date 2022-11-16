2eaaf4e886be40013550848eda5408f0009e3c23
kramirez8808 <kramirez8808@gmail.com>
2022-11-15 18:58:45 -0600

    First Commit Doctor.java

--- practicaVeterinaria/src/practicaveterinaria/Doctor.java
+++ practicaVeterinaria/src/practicaveterinaria/Doctor.java
@@ -9,5 +9,68 @@ package practicaveterinaria;
  * @author fanta
  */
 public class Doctor {
+    //Atributos
+    private String nombre;
+    private String apellidos;
+    private String cedula;
+    private String IDdoctor;
+    private String numeroSede;
+
+    //Constructor vacio 
+    public Doctor() {
+
+    }
     
+    //Constructor
+    public Doctor(String nombre, String apellidos, String cedula, String IDdoctor, String numeroSede) {
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.cedula = cedula;
+        this.IDdoctor = IDdoctor;
+        this.numeroSede = numeroSede;
+    }
+
+    //GETTERS
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public String getCedula() {
+        return cedula;
+    }
+
+    public String getIDdoctor() {
+        return IDdoctor;
+    }
+
+    public String getNumeroSede() {
+        return numeroSede;
+    }
+
+    //SETTERS
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
+
+    public void setIDdoctor(String IDdoctor) {
+        this.IDdoctor = IDdoctor;
+    }
+
+    public void setNumeroSede(String numeroSede) {
+        this.numeroSede = numeroSede;
+    }
+
 }
